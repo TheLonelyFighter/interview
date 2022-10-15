da23e27d6e271f0e69f73c92b7c93ce9a2688045
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-15 20:34:44 +0300

    vector list methods

--- src/arrays.cpp
+++ src/arrays.cpp
@@ -7,8 +7,12 @@ int main()
     std::vector<int> v = {3,5,2,1,4};
 
     std::cout << "Initial vector" << std::endl;
-    for (auto i : v)
+    for (auto& i : v)
         std::cout << i << std::endl;
+
+    //remove element with value 4
+    v.erase(std::remove(v.begin(), v.end(), 4), v.end());
+
 
     // sort the vector in descending order
     std::sort(v.begin(), v.end(), std::greater<int>());
@@ -17,6 +21,6 @@ int main()
     std::reverse(v.begin(), v.end());
     
     std::cout << "After sort and reverse" << std::endl;
-    for (auto i : v)
+    for (auto& i : v)
         std::cout << i << std::endl;
 }
--- src/class_example.cpp
+++ src/class_example.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
 
 class Cars {
@@ -35,11 +37,66 @@ void Cars::outside_method()
     std::cout << "This is a method defined outside the class" << std::endl;
 }
 
-int main() {
-    Cars car_one("BMW", 300, 6);
-    std::cout << car_one.get_speed() << std::endl;
+int main() 
+{
+    //static declaration
+    Cars bmw("BMW", 300, 6);
+    Cars nissan("Nissan", 250, 7);
+    Cars dacia("Dacia", 160, 5);
+    std::vector<Cars> car_list = {bmw, nissan, dacia};
+    std::cout << bmw.get_speed() << std::endl;
     int max_speed = 200;
-    car_one.set_speed(max_speed);
-    std::cout << car_one.brand << " " << car_one.get_speed() << std::endl;
-    car_one.outside_method();
+    bmw.set_speed(max_speed);
+    std::cout << bmw.brand << " " << bmw.get_speed() << std::endl;
+    bmw.outside_method();
+
+    //sort example using lambda function
+    std::sort(car_list.begin(), car_list.end(), [](Cars a, Cars b) {return a.get_speed() > b.get_speed();});
+
+    for(auto i : car_list) {
+        std::cout << i.brand << " " << i.get_speed() << std::endl;
+    }
+
+    Cars("Ferarri", 400, 8);
+    car_list.push_back(Cars("Ferarri", 400, 8));
+    std::sort(car_list.begin(), car_list.end(), [](Cars a, Cars b) {return a.get_speed() < b.get_speed();});
+    for(auto i : car_list) {
+        std::cout << i.brand << " " << i.get_speed() << std::endl;
+    }
+
+    //dynamic declaration
+    Cars *audi = new Cars("Audi", 280, 6);
+    Cars *skoda = new Cars("Skoda", 200, 6);
+
+    std::vector<Cars> car_list_dyn = {*audi, *skoda};
+
+
+    std::cout << "\nDynamic declaration" << std::endl;
+    Cars *mercedes = new Cars("Mercedes", 320, 7);
+    car_list_dyn.push_back(*mercedes);
+    std::sort(car_list_dyn.begin(), car_list_dyn.end(), [](Cars a, Cars b) {return a.get_speed() > b.get_speed();});
+
+    for(auto i : car_list_dyn) {
+        std::cout << i.brand << " " << i.get_speed() << std::endl;
+    }
+
+    //car_list_dyn.erase(car_list_dyn.begin());
+
+    for(auto& i : car_list_dyn) {
+        std::cout << i.brand << " " << i.get_speed() << std::endl;
+    }
+    int index = 0;
+    for (auto& i : car_list_dyn) {
+        
+        if (i.brand == "Mercedes") {
+            std::cout << "Mercedes is in the list, will be deleted" << std::endl;
+            car_list_dyn.erase(car_list_dyn.begin() + index);
+        }
+        index++;
+    }
+
+    for(auto& i : car_list_dyn) {
+        std::cout << i.brand << " " << i.get_speed() << std::endl;
+    }
+    return 0;
 }
