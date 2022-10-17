d52a2f927f9ba8b5fc01c59b17eea611878f1741
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-17 17:39:23 +0300

    additional comments

--- src/smart_pointers.cpp
+++ src/smart_pointers.cpp
@@ -5,14 +5,18 @@
 int main() {
     
     
-    int* int_p_raw;
+    int* int_p_raw = nullptr;
 
-    std::shared_ptr<int> int_p_unique;
+    std::shared_ptr<int> int_p_unique = std::make_shared<int>(5);
     //std::cout << "int_p_unique: " << int_p_unique.get() << " value: " << *int_p_unique <<std::endl;
 
     // c++ allows to use unintiliazed pointer --> it is very bad practice
-    std::cout << "int_p_raw: " << int_p_raw << " value: " << *int_p_raw <<std::endl;
-    
+    if (int_p_raw != nullptr) {
+        std::cout << "int_p_raw: " << int_p_raw << " value: " << *int_p_raw <<std::endl;
+    }
+    else {
+        std::cout << "int_p_raw is nullptr" << std::endl;
+    }
     // smart pointers are initialized with nullptr, so trying to access them will result in segfault
     if (int_p_unique) {
         std::cout << "int_p_unique: " << int_p_unique.get() << " value: " << *int_p_unique <<std::endl;
@@ -21,17 +25,17 @@ int main() {
         std::cout << "int_p_unique is null" << std::endl;
     }
 
-    // smart pointers are destroyed after being deallocated, phew 
+    // smart pointers are deleted automatcally after being deallocated, phew no more memory leaks
     int_p_unique.reset();
 
-    //std::unique_ptr<std::string> song_smart(new std::string(L"Nothing on You", L"Bruno Mars"));
-
-    // Use song2...
-    //std::cout << *song_smart << std::endl;
-    //...
-    // Don't forget to delete!
-   // delete song_raw;  
+    
+    // Don't forget to delete raw pointers!
+    // If you try to delete a smart pointer, you will get a compile error
+    // If you try to delete an uninitialized pointer, you will get a segfault, always use nullptr
+    delete int_p_raw;  
     return 0;
 }
 
-// song2 is deleted automatically here.
+// int_p_unique is deleted automatically here.
+
+// Lesson learned: just don't use raw pointers, use smart pointers instead.
