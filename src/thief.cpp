f5b2df857f1b451730f4e6f904049dbb7ac5aabd
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-20 15:55:41 +0300

    initialized J with lower values

--- src/thief.cpp
+++ src/thief.cpp
@@ -5,29 +5,34 @@ int main () {
     // {100,120, 100, 90}
     // {100,      120,       100,     50}
     // {100,       90,         90,      100}
+    // { 6, 7, 1, 3, 8, 2, 4 }
 
-    std::vector<int> houses =  {100,120, 100, 90, 1000, 1000};
+    std::vector<int> houses =  {6, 7, 2};
 
-    int max = 0;
-    int J3 = 0;
+    if (houses.size() == 0) {
+        std::cout << "0" << std::endl;
+        return 0;
+    }
+    else if (houses.size() == 1) {
+        std::cout << houses[0] << std::endl;
+        return 0;
+    }
+
     int J2 = 0;
-    int J1 = 0;
-
+    int J1 = houses[0];;
+    int J0 = 0;
      
-    J1 = houses[0];
-    J2 = std::max(J1, houses[1]);
-
-    for (int i = 2; i < houses.size(); i++)  {   
+    for (int i = 1; i < houses.size(); i++)  {   
      
-        J3 = std::max(J2, J1 + houses[i]);
+        J2 = std::max(J1, J0 + houses[i]);
+        J0 = J1;
         J1 = J2;
-        J2 = J3;
 
-        std::cout << "Current max: " << J3 << std::endl;
+        std::cout << "Current max: " << J2 << std::endl;
 
         }
 
-    std::cout << "Max cash: " <<  J3 << std::endl;
+    std::cout << "Max cash: " <<  J2 << std::endl;
 
     return 0;
 }
