d5864fa0661d548036f19081d9d79b5918a24b04
TheLonelyFighter <mihaiandrei1987@yahoo.com>
2022-10-20 16:08:52 +0300

    added description of the problem

--- src/thief.cpp
+++ src/thief.cpp
@@ -1,13 +1,31 @@
 #include<iostream>
 #include<vector>
 
+
+/* Problem proposed during Rapyuta's programming interview. 
+
+There are N houses built in a line, each of which contains some value in it.
+A thief is going to steal the maximal value of these houses, 
+but he can’t steal in two adjacent houses because 
+the owner of the stolen houses will tell his two neighbors left and right sides. 
+The task is to find what is the maximum stolen value.
+
+Input: hval[] = {6, 7, 1, 3, 8, 2, 4}
+Output: 19
+Explanation: The thief will steal 6, 1, 8 and 4 from the house.
+
+Input: hval[] = {5, 3, 4, 11, 2}
+Output: 16
+Explanation: Thief will steal 5 and 11
+*/
 int main () {
     // {100,120, 100, 90}
     // {100,      120,       100,     50}
     // {100,       90,         90,      100}
+    // {5, 3, 4, 11, 2}
     // { 6, 7, 1, 3, 8, 2, 4 }
 
-    std::vector<int> houses =  {6, 7, 2};
+    std::vector<int> houses =  { 6, 7, 1, 3, 8, 2, 4 };
 
     if (houses.size() == 0) {
         std::cout << "0" << std::endl;
